6349bf8cbf183ef89e8abf85f5cb2e58e7526ce9
mwiesiolek <maksymilian.wiesiolek@gmail.com>
2017-10-21 11:59:44 +0200

    authorization header + auth module

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,15 +1,14 @@
-import { Http } from '@angular/http';
-import { Injectable } from '@angular/core';
-import 'rxjs/add/operator/map';
+import {Injectable} from "@angular/core";
+import "rxjs/add/operator/map";
+import {AuthHttp} from "angular2-jwt";
+import {Http} from "@angular/http";
 
 @Injectable()
 export class OrderService {
+  constructor(private authHttp: AuthHttp, private http: Http) {}
 
-  constructor(private http: Http) {
-  }
-
-  getOrders() { 
-    return this.http.get('/api/orders')
+  getOrders() {
+    return this.authHttp.get('/api/orders')
       .map(response => response.json());
   }
 }
